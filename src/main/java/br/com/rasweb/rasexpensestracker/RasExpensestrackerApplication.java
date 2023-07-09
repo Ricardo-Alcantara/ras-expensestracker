14ef4cd02f66b8bb288309ad855a753dd385a228
Ricardo Alcantara <ricardo.alcantara.silva@gmail.com>
2023-07-09 16:55:03 -0300

    Initial commit

---
+++ src/main/java/br/com/rasweb/rasexpensestracker/RasExpensestrackerApplication.java
@@ -0,0 +1,13 @@
+package br.com.rasweb.rasexpensestracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RasExpensestrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RasExpensestrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rasweb/rasexpensestracker/RasExpensestrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rasweb.rasexpensestracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RasExpensestrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
